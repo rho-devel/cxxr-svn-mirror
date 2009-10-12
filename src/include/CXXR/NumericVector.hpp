8b2715a7403891b35b7742a58f588a5044e3384a
verafice <unknown@unkown.com>
2009-10-12 11:57:07 +0000

    Added NumericVector specilisations and a test stub.
    git-svn-id: http://projects.cs.kent.ac.uk/projects/cxxr/svn/branches/snicholls/templated_arithmetic/0.23-2.9.2@663 549f2c98-2231-0410-b618-a09cd5e121c0

--- src/include/CXXR/NumericVector.hpp
+++ src/include/CXXR/NumericVector.hpp
@@ -2,28 +2,75 @@
 #define NUMERICVECTOR_HPP 1
 
 #include "CXXR/DumbVector.hpp"
-
+#include <limits>
 namespace CXXR{
     template <typename T, SEXPTYPE ST>
-    class NumericVector : public DumbVector {
+    class NumericVector : public DumbVector<T,ST>{
         public:
-            NumericVector()
-                    : DumbVector<T,ST>()
-            {
-            }
+            NumericVector() : DumbVector<T,ST>(){}
 
-            //! Returns the appropriate internal NA value for type T.
-            static T NA_value()
-            {
-                //find values of:
-                //NA_REAL = R_ValueOfNA() - see arithmetic.cpp
-                //NA_INTEGER = INT_MIN
-                return 0; //temporary...
-            }
-
+            //! Returns NA value for the template type. See specilisations.
+            static T NA_value(){return NULL; /*maybe throw an exception instad.*/ }
+	    
+	    class Subtract{
+	    	public:
+		    Subtract(){};
+		    ~Subtract(){};
+		    static T op(T l, T r){return NULL;}
+	    };
         private:
+	    //! private deconstructor prevents creation on the stack.
             virtual ~NumericVector(){ }
+	    //! definitions used with real numbers.
+	    typedef union
+	    {
+	        double value;
+		unsigned int word[2];
+	    } ieee_double;
+	    #ifdef WORDS_BIGENDIAN
+	    static const int ieee_hw = 0;
+	    static const int ieee_lw = 1;
+	    #else  /* !WORDS_BIGENDIAN */
+	    static const int ieee_hw = 1;
+	    static const int ieee_lw = 0;
+	    #endif /* WORDS_BIGENDIAN */
     };
+    //! Specilisation for int.
+    template<>
+    int NumericVector<int,INTSXP>::NA_value(){
+    	return INT_MIN;
+    }
+    
+    //! Specilisation for double.
+    template<>
+    double NumericVector<double,REALSXP>::NA_value(){
+	volatile ieee_double x;
+	x.word[ieee_hw] = 0x7ff00000;
+        x.word[ieee_lw] = 1954;
+	return x.value;
+    }
+
+    //! Naive subtract specilisation for int.
+    template<>
+    int NumericVector<int,INTSXP>::Subtract::op(int l, int r){
+	// TODO: handling for range.
+	if(l==NumericVector<int,INTSXP>::NA_value() ||
+		r==NumericVector<int,INTSXP>::NA_value()){
+            return NumericVector<int,INTSXP>::NA_value();
+	}
+	return l-r;
+    }
+
+    //! Naive subtract specilisation for double.
+    template<>
+    double NumericVector<double,REALSXP>::Subtract::op(double l, double r){
+	//TODO: handling for range.
+	if(l==NumericVector<double,REALSXP>::NA_value() ||
+	        r==NumericVector<double,REALSXP>::NA_value()){
+	    return NumericVector<double,REALSXP>::NA_value();
+	}
+	return l-r;
+    }
 }
 
 #endif
---
+++ tests/CXXR/NumericVector.cpp
@@ -0,0 +1,12 @@
+#if 0
+g++ -o NumericVector -I../../src/include NumericVector.cpp
+exit;
+#endif
+
+#include <iostream>
+#include <cstdlib>
+#include "CXXR/NumericVector.hpp"
+int main(){
+    return EXIT_SUCCESS;
+}
+
