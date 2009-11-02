1e0edbdc7dd3dd2f5675f492b93e91d82c85ba72
verafice <unknown@unkown.com>
2009-11-02 13:00:35 +0000

    doxygen comments improved.
    git-svn-id: http://projects.cs.kent.ac.uk/projects/cxxr/svn/branches/snicholls/templated_arithmetic/0.23-2.9.2@697 549f2c98-2231-0410-b618-a09cd5e121c0

--- src/include/CXXR/NumericVector.h
+++ src/include/CXXR/NumericVector.h
@@ -1,3 +1,8 @@
+/**
+ * @file NumericVector.h
+ * @brief Class CXXR::NumericVector
+ */
+
 #ifndef NUMERICVECTOR_HPP
 #define NUMERICVECTOR_HPP 1
 
@@ -7,25 +12,50 @@ namespace CXXR{
 	template <typename T, SEXPTYPE ST>
 	class NumericVector : public DumbVector<T,ST>{
 	public:
+		/**
+		 * @brief Constructs an uninitialized \c NumericVector of size sz.
+		 * @param sz Size to make the \c NumericVector, may be zero.
+		 */
 		NumericVector(size_t sz);
+		/**
+		 * @brief Constructs an initialized \c NumericVector of size sz.
+		 * @param sz Size to make the \c NumericVector, may be zero.
+		 * @param initializer Every element of the \c NumericVector will be set to this value.
+		 */
 		NumericVector(size_t sz, const T& initializer);
-		//! Returns NA value for the template type.
+		/**
+		 * @brief Returns the value used internally to represent NA for the given instantiation of \c NumericVector .
+		 * @return NA value for type T.
+		 */
 		static T NA_value();
 		
-		// Returns the internal type name of the current type.
-		//(this is abstract in the base class.)
+		/**
+		 * @brief Returns the internal type name used by a given instantiation of \c NumericVector .
+		 * @return An internal type name.
+		 */
 		static const char* staticTypeName();
-		
+		/**
+		 * @brief Class CXXR::NumericVector::Subtract implements a subtract operation between two \c NumericVector classes.
+		 */
 		class Subtract{
 			public:
 			Subtract();
 			virtual ~Subtract();
+			/**
+			 * @brief op implements the subtraction operation between two values of type T.
+			 * @param l the left \c value for the operation l-r.
+			 * @param r the right \c value for the operation l-r.
+			 * @return the result of l-r. If either l or r were NA, the result is NA. 
+			 * 			If neither were NA, but the result is NA, an error has occured.
+			 */
 			static T op(T l, T r);
 		};
 	private:
-		//! private deconstructor prevents creation on the stack.
+		/**
+		 * @brief Private deconstructor; Prevents creation on the stack.
+		 */
 		virtual ~NumericVector();
-		//! definitions used with real numbers.
+		//definitions used to manipulate real numbers in an endian-safe way.
 		typedef union{
 			double value;
 			unsigned int word[2];
@@ -37,6 +67,7 @@ namespace CXXR{
 		static const int ieee_hw = 1;
 		static const int ieee_lw = 0;
 		#endif /* WORDS_BIGENDIAN */
+		//definitions used to perform range-checks on integers.
 		static inline bool GOODISUM(int x, int y, int z);
 		static inline bool GOODIPROD(int x,int y, int z);
 		static inline bool OPPOSITE_SIGNS(int x, int y);
