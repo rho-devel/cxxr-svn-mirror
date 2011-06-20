88e2671a533b2b200c6ec9d8de7697aa5f4db97d
Andrew Runnalls <a.r.runnalls@kent.ac.uk>
2011-06-20 10:54:14 +0000

    do_mapply() reimplemented in a way compatible with lazycopy.
    git-svn-id: http://projects.cs.kent.ac.uk/projects/cxxr/svn/branches/lazycopy@1157 549f2c98-2231-0410-b618-a09cd5e121c0

--- src/main/mapply.cpp
+++ src/main/mapply.cpp
@@ -39,83 +39,89 @@
 
 #include <Defn.h>
 #include <basedecl.h>
+#include "CXXR/GCStackRoot.hpp"
+
+using namespace CXXR;
 
 SEXP attribute_hidden
 do_mapply(SEXP f, SEXP varyingArgs, SEXP constantArgs, SEXP rho)
 {
 
-    int i, j, m,nc, *lengths, *counters, named, longest=0;
-    SEXP vnames, fcall = R_NilValue,  mindex, nindex, tmp1, tmp2, ans;
+    int m,nc, *lengths, *counters, named, longest=0;
 
     m = length(varyingArgs);
     nc = length(constantArgs);
-    vnames = PROTECT(getAttrib(varyingArgs, R_NamesSymbol));
+    GCStackRoot<> vnames(getAttrib(varyingArgs, R_NamesSymbol));
 
     named = vnames!=R_NilValue;
 
     lengths = static_cast<int *>(CXXR_alloc(m, sizeof(int)));
-    for(i = 0; i < m; i++){
+    for(int i = 0; i < m; i++){
 	lengths[i] = length(VECTOR_ELT(varyingArgs,i));
 	if (lengths[i] > longest) longest=lengths[i];
     }
 
 
     counters = static_cast<int *>(CXXR_alloc(m, sizeof(int)));
-    for(i = 0; i < m; counters[i++]=0);
+    for(int i = 0; i < m; counters[i++]=0);
 
-    mindex=PROTECT(allocVector(VECSXP, m));
-    nindex=PROTECT(allocVector(VECSXP, m));
+    GCStackRoot<> mindex(allocVector(VECSXP, m));
 
     /* build a call
        f(dots[[1]][[4]],dots[[2]][[4]],dots[[3]][[4]],d=7)
     */
+    // In CXXR:
+    // f(dots[[1]][[ nindex[1] ]], dots[[2]][[ nindex[2] ]],...
 
+    GCStackRoot<> fcall;
     if (constantArgs == R_NilValue)
-	PROTECT(fcall=R_NilValue);
+	;
     else if(isVectorList(constantArgs))
-	PROTECT(fcall=VectorToPairList(constantArgs));
+	fcall=VectorToPairList(constantArgs);
     else
 	error(_("argument 'MoreArgs' of 'mapply' is not a list"));
 
-    for(j = m-1; j >= 0; j--) {
+    Symbol* nindexsym = Symbol::obtain("nindex");
+    for(int j = m-1; j >= 0; j--) {
 	SET_VECTOR_ELT(mindex, j, ScalarInteger(j+1));
-	SET_VECTOR_ELT(nindex, j, allocVector(INTSXP, 1));
 
-	PROTECT(tmp1=lang3(R_Bracket2Symbol,
-			   install("dots"),
-			   VECTOR_ELT(mindex, j)));
-
-	PROTECT(tmp2=lang3(R_Bracket2Symbol,
-			   tmp1,
-			   VECTOR_ELT(nindex, j)));
-
-
-	UNPROTECT(3);
-	PROTECT(fcall=CONS(tmp2, fcall));
+	GCStackRoot<> tmp1(lang3(R_Bracket2Symbol,
+				 install("dots"),
+				 VECTOR_ELT(mindex, j)));
+	GCStackRoot<> tmp1a(lang3(R_BracketSymbol,
+				  nindexsym, VECTOR_ELT(mindex, j)));
+	GCStackRoot<> tmp2(lang3(R_Bracket2Symbol,
+				 tmp1, tmp1a));
+	fcall=CONS(tmp2, fcall);
 
 	if (named && CHAR(STRING_ELT(vnames,j))[0]!='\0')
 	    SET_TAG(fcall, install(translateChar(STRING_ELT(vnames,j))));
 
     }
-    UNPROTECT(1);
-    PROTECT(fcall=LCONS(f, fcall));
+    fcall=LCONS(f, fcall);
 
-    PROTECT(ans=allocVector(VECSXP, longest));
+    GCStackRoot<IntVector> nindex(CXXR_NEW(IntVector(m)));
+    // Set up binding of nindex in rho:
+    {
+	Environment* env = SEXP_downcast<Environment*>(rho);
+	Frame::Binding* bdg = env->frame()->obtainBinding(nindexsym);
+	bdg->setValue(nindex);
+    }
+    
+    GCStackRoot<> ans(allocVector(VECSXP, longest));
 
-    for(i = 0; i < longest; i++) {
-	for(j = 0; j < m; j++) {
+    for(int i = 0; i < longest; i++) {
+	for(int j = 0; j < m; j++) {
 	    counters[j] = (++counters[j]>lengths[j]) ? 1 : counters[j];
-	    INTEGER(VECTOR_ELT(nindex,j))[0] = counters[j];
+	    (*nindex)[j] = counters[j];
 	}
 	SET_VECTOR_ELT(ans, i, eval(fcall, rho));
     }
 
-    for(j = 0; j < m; j++) {
+    for(int j = 0; j < m; j++) {
 	if (counters[j] != lengths[j])
 	    warning(_("longer argument not a multiple of length of shorter"));
     }
-
-    UNPROTECT(5);
 
     return(ans);
 }
